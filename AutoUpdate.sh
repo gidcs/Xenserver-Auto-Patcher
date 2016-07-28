483e9749d319b6297bf562dddea80689be6e1828
Lim Kok Suan <admin@gidcs.net>
2016-07-29 00:11:38 +0800

    AutoUpdate.sh for all version

---
+++ AutoUpdate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# AutoUpdateAllPatch for XenServer
+# Powered by LKS
+# http://www.guyusoftware.com/
+#
+# AutoUpdate.sh
+
+./AutoUpdatePatchFile.sh 56
+./AutoUpdatePatchFile.sh 62
+./AutoUpdatePatchFile.sh 65
+./AutoUpdatePatchFile.sh 7
